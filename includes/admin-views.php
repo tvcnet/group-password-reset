002dba45d882ddf90a3a24b27850d0bcfdc5819c
tvcnet <tvcnet@users.noreply.github.com>
2026-03-30 18:38:40 -0700

    Restyle modal donation action

--- includes/admin-views.php
+++ includes/admin-views.php
@@ -106,9 +106,11 @@ function gpr_render_plugin_details_modal() {
 			</div>
 
 			<div class="gpr-plugin-modal__footer">
-				<a class="button button-primary" href="<?php echo esc_url( gpr_get_download_url() ); ?>" target="_blank" rel="noopener"><?php esc_html_e( 'Download Plugin', 'group-password-reset' ); ?></a>
-				<a class="button" href="<?php echo esc_url( GPR_PLUGIN_URI ); ?>" target="_blank" rel="noopener"><?php esc_html_e( 'Plugin Homepage', 'group-password-reset' ); ?></a>
-				<a class="button" href="https://hackrepair.com/donations/buy-jim-a-coffee" target="_blank" rel="noopener"><?php esc_html_e( 'Donate to this plugin »', 'group-password-reset' ); ?></a>
+				<div class="gpr-plugin-modal__footer-actions">
+					<a class="button button-primary" href="<?php echo esc_url( gpr_get_download_url() ); ?>" target="_blank" rel="noopener"><?php esc_html_e( 'Download Plugin', 'group-password-reset' ); ?></a>
+					<a class="button" href="<?php echo esc_url( GPR_PLUGIN_URI ); ?>" target="_blank" rel="noopener"><?php esc_html_e( 'Plugin Homepage', 'group-password-reset' ); ?></a>
+				</div>
+				<a class="button gpr-plugin-modal__donate-button" href="https://hackrepair.com/donations/buy-jim-a-coffee" target="_blank" rel="noopener"><?php esc_html_e( 'Donate to this plugin »', 'group-password-reset' ); ?></a>
 			</div>
 		</div>
 	</div>
